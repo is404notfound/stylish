f7f51e8f0971781b3b689a7405fbd659c51259a2
is404notfound <this.console.log@gmail.com>
2025-01-03 07:23:33 +0900

    [Fix] router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import Components from './pages/Components';
 
-
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/stylish" element={<Home />} />
           <Route path="/components" element={<Components />} />
         </Routes>
       </BrowserRouter>
