f160b3ecf5746e2ff5dc25696867a2f239c3cac0
is404notfound <this.console.log@gmail.com>
2025-01-03 05:52:40 +0900

    [Add] Drawer 컴포넌트 추가

---
+++ src/components/Drawer.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useRef, useEffect } from 'react';
+import '../styles/components/drawer.css';
+
+const Drawer = ({ children }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const timeoutRef = useRef(null); 
+      const drawerRef = useRef(null);
+  
+    useEffect(() => {
+      const handleMouseMove = (e) => {
+        if (e.clientX < 50) {
+          clearTimeout(timeoutRef.current);
+          timeoutRef.current = setTimeout(() => {
+            setIsOpen(true);
+          }, 300);
+        } else {
+          clearTimeout(timeoutRef.current);
+          timeoutRef.current = setTimeout(() => {
+            setIsOpen(false);
+          }, 300);
+        }
+      };
+  
+      window.addEventListener('mousemove', handleMouseMove);
+  
+      return () => {
+        window.removeEventListener('mousemove', handleMouseMove);
+        clearTimeout(timeoutRef.current);
+      };
+    }, []);
+  
+      const handleClose = () => {
+          setIsOpen(false);
+      }
+  
+    return (
+      <>
+        <div className={`drawer ${isOpen ? 'open' : ''}`} ref={drawerRef}>
+          <nav>
+            <ul>
+              {children}
+            </ul>
+          </nav>
+        </div>
+      </>
+    );
+  };
+  
+  export default Drawer;
